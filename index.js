36f7376f564ac7e87ab3fea16c68371818ef80b4
DinacoStudio <saitovmirat27@gmail.com>
2023-06-02 21:59:15 +0500

    8921

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express();
-
 app.get("/watch", (req, res) => {
     return res.redirect(`https://work.220youtube.ru/youtube/${req.query['v']}/download/22?title=test`);
 })
