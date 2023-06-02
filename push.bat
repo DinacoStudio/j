68b0477abeaf9f67b9e04e721773adcddcceeb3f
DinacoStudio <saitovmirat27@gmail.com>
2023-06-02 21:59:05 +0500

    8888

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 @echo off
 git commit -a -m %random%
-git remote add origin https://github.com/DinacoStudio/youtube.git
+git remote add origin https://github.com/DinacoStudio/j.git
 git branch -M main
 git push -u origin main
 pause
